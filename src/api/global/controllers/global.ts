70a7aa19550658ba640f305d455cfa8bfffa6ef6
shaheer79 <shaheershaikh79@gmail.com>
2025-07-16 01:34:39 +0500

    pushed

--- src/api/global/controllers/global.ts
+++ src/api/global/controllers/global.ts
@@ -1,7 +1,15 @@
-/**
- * global controller
- */
+import { factories } from '@strapi/strapi';
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController('api::global.global', ({ strapi }) => ({
+  async find(ctx) {
+    const entry = await strapi.entityService.findOne('api::global.global', 1, {
+      populate: {
+        header: { populate: '*' },
+        footer: { populate: '*' },
+        logo: { populate: '*' },
+      },
+    });
 
-export default factories.createCoreController('api::global.global');
+    return this.transformResponse(entry);
+  },
+}));
